7b62ce84b8894d11cff8c9bf4d12684bac4509a1
Gucci Fan <gucc@umich.edu>
2022-07-09 16:48:50 -0400

    commit tree 94e532bcb0ed4d3de95db78ade58088e9d0f9385

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(drivers, name){
+    const matchingNames = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return matchingNames
+}
+function fuzzyMatch(drivers, letters){
+    const fuzzyNames = drivers.filter(driver => driver.startsWith(letters))
+    return fuzzyNames
+}
+function matchName(drivers, name){
+    const matchedName = drivers.filter(driver => driver.name === name)
+    return matchedName
+}
